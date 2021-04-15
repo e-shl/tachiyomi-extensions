6d77b40e5adcc81ce01a6f58e57e5e59d3dc00b1
e-shl <e.shlyapkin99@gmail.com>
2021-04-15 21:48:57 +0500

    Fix queue autoServer

--- src/ru/libmanga/src/eu/kanade/tachiyomi/extension/ru/libmanga/LibManga.kt
+++ src/ru/libmanga/src/eu/kanade/tachiyomi/extension/ru/libmanga/LibManga.kt
@@ -301,7 +301,7 @@ class LibManga : ConfigurableSource, HttpSource() {
         val chapInfoJson = jsonParser.parse(chapInfo).obj
         val servers = chapInfoJson["servers"].asJsonObject.toMap()
         val defaultServer: String = chapInfoJson["img"]["server"].string
-        val autoServer = setOf(defaultServer, "secondary", "fourth", "compress")
+        val autoServer = setOf("secondary", "fourth", defaultServer, "compress")
         val imgUrl: String = chapInfoJson["img"]["url"].string
 
         val serverToUse = when (this.server) {
@@ -347,7 +347,6 @@ class LibManga : ConfigurableSource, HttpSource() {
         if (urls.size == 1) {
             return Observable.just(urls[0])
         }
-
         return Observable.from(urls).filter { checkImage(it) }.first()
     }
 
